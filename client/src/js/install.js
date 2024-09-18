f0bf3f5e692d7bb3ec2e88c9ec3c2cb08902e7d9
Paul Baumert <baumert.trey@gmail.com>
2024-09-18 11:31:26 -0500

    updated install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,38 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
+let deferredPrompt;
+
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent the default mini-infobar from appearing
+  event.preventDefault();
+  
+  // Store the event so it can be triggered later
+  deferredPrompt = event;
+
+  // Remove the hidden class from the install button
+  butInstall.style.display = 'block';
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt(); // Show the install prompt
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+    // Wait for the user to respond to the prompt
+    const { outcome } = await deferredPrompt.userChoice;
+    console.log(`User response to the install prompt: ${outcome}`);
+
+    // Reset the deferredPrompt variable since it can only be used once
+    deferredPrompt = null;
+
+    // Hide the install button after installation
+    butInstall.style.display = 'none';
+  }
+});
+
+// TODO: Add a handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+  console.log('PWA was installed', event);
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,27 +3,68 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: 'development', // Development mode
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: './src/js/index.js',    // Main entry point
+      install: './src/js/install.js' // For handling PWA install logic
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].bundle.js', // Dynamically name bundles
+      path: path.resolve(__dirname, 'dist'), // Output directory
     },
     plugins: [
-      
+      // Generates the main HTML file
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE', // Customize the title
+      }),
+      // Injects the custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js', // Path to service worker source file
+        swDest: 'src-sw.js',  // Destination in the dist folder
+      }),
+      // PWA Manifest
+      new WebpackPwaManifest({
+        fingerprints: false, // Avoids hashing the file names
+        inject: true, // Injects the manifest link into the HTML
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'A simple text editor that works offline!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512], // Multiple icon sizes
+            destination: path.join('assets', 'icons'), // Where icons will be stored
+          },
+        ],
+      }),
     ],
 
+    // Define module rules
     module: {
       rules: [
-        
+        // CSS loader
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        // Babel loader for transpiling JavaScript
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'], // Use Babel preset-env
+            },
+          },
+        },
       ],
     },
   };
